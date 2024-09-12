9177f22646a48905de9232accd608384bf9c62eb
AnujMishra <anujm77000@gmail.com>
2024-09-12 10:12:18 +0530

    update test cases

--- Note_app/tests.py
+++ Note_app/tests.py
@@ -1,53 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from Note_app.models import Notes
 
-class Create_NoteApiTest_Case(APITestCase):
+from Note_app.Api import serializers
+from Note_app import models
 
-    def test_create_note(self):
+class CreateNoteViewTestCase(APITestCase):
+    
+    def setUp(self):
+        self.title = models.Notes.objects.create(title="note one", body="it first note")
         
-        url = reverse('Creating-note')  
+
+    def test_createnote(self):
         data = {
-            'title': 'Test Note',
-            'body': 'This is a test Note.'
+            "title":"hello",
+            "body":"hello i am test file"
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(reverse('Creating-Note'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Notes.objects.count(), 1)
-        self.assertEqual(Notes.objects.get().title, 'Test Note')
 
+    def test_NoteById(self):
+        response = self.client.get(reverse('Fetching_Note', args=(self.title.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.Notes.objects.count(), 1)
+        self.assertEqual(models.Notes.objects.get().title,"note one")
+    
+    def test_NoteByTitle(self):
+        search_term='Test'
+        response= self.client.get(reverse('Search_by_Query'),{'search':search_term})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_fetch_note_by_id(self):
-       
-        note = Notes.objects.create(title='Test Note', body='This is a test note.')
-        url = reverse('Fetching-Note', args=[note.id])  
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Test Note')
-   
-    def test_query_notes_by_title_substring(self):
-        
-        Notes.objects.create(title='Important Note', body='This is an important note.')
-        Notes.objects.create(title='Another Note', body='Just another note.')
-        url = reverse('Search_by_Query')  
-        response = self.client.get(url, {'search': 'title_name'})  
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], 'Existing Note')
- 
-    def test_update_note(self):
-        
-        note = Notes.objects.create(title='Old Title', body='Old body text.')
-        url = reverse('Updating-Note', args=[note.id])  
+    def test_UpdateNote(self):
         data = {
-            'title': 'Updated Title',
-            'body': 'Updated body text.'
+            "title":"hello",
+            "body":"hello i am Updated test file"
         }
-        response = self.client.put(url, data, format='json')
+        response = self.client.put(reverse('Updating-Note', args=(self.title.id,)), data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        note.refresh_from_db()
-        self.assertEqual(note.title, 'Updated Title')
-        self.assertEqual(note.body, 'Updated body text.')
-
